12a2ddf36adb7ce53f04b8f534d2844eeb51066f
qthwng01 <qthwng.01@gmail.com>
2025-08-24 23:26:31 +0700

    update: index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from my container! 🐳");
+  res.send("Hello from my container and running in Render Cloud! 🐳");
 });
 
 app.listen(port, () => {

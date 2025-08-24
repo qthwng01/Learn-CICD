3661969888196be0957edab179598a52c014d8a4
qthwng01 <qthwng.01@gmail.com>
2025-08-25 00:25:02 +0700

    Update: file .yml & index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from my container and running in Render Cloud & Docker Image! 🐳");
+  res.send("Auto Deloy using Webhook Render! 🐳");
 });
 
 app.listen(port, () => {
